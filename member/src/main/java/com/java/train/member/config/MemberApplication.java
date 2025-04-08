cf1ddad9e1381a4d0005030919409b95576bf601
Harmony-Lu <3292947704@qq.com>
2025-04-08 21:02:56 +0800

    修改启动日志，添加/member路由，修改LogBack日志

--- member/src/main/java/com/java/train/member/config/MemberApplication.java
+++ member/src/main/java/com/java/train/member/config/MemberApplication.java
@@ -3,13 +3,22 @@ package com.java.train.member.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @ComponentScan("com.java") // 帮助扫描到controller包中的文件
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+//        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("测试地址：\thttp://127.0.0.1:{}{}", env.getProperty("server.port"), env.getProperty("server.servlet..context-path"));
     }
 
 }
