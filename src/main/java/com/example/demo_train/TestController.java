41b463e1bd98950a5deacd0d136ff9741f16e337
Harmony-Lu <3292947704@qq.com>
2025-04-08 16:41:27 +0800

    thrid commit

--- src/main/java/com/example/demo_train/TestController.java
+++ src/main/java/com/example/demo_train/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello world, my friend!";
+        return "hello world, my friend Harmony!";
     }
 
 }
