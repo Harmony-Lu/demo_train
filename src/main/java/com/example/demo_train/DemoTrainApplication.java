10229538cb9e55167ebdfeff394673bb76287c08
Harmony-Lu <3292947704@qq.com>
2025-04-08 16:24:38 +0800

    first commit

---
+++ src/main/java/com/example/demo_train/DemoTrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTrainApplication.class, args);
+    }
+
+}
