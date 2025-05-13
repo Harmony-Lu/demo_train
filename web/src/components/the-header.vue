aef3c7826d4fe37c67e12d649727041a56a4950d
Harmony-Lu <3292947704@qq.com>
2025-05-13 21:32:39 +0800

    vue组件开发，增加the-header和the-sider组件

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 11</a-menu-item>
+      <a-menu-item key="2">nav 22</a-menu-item>
+      <a-menu-item key="3">nav 33</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import {defineComponent, reactive, ref} from 'vue';
+
+export default defineComponent({
+  name: "the-header-view",
+  setup() {
+
+    return {
+      selectedKeys1 : ref(['2'])
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
