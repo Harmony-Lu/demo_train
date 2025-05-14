22835ca1d1238295922e0c8f1a8bc551968d3f31
Harmony-Lu <3292947704@qq.com>
2025-05-14 15:04:59 +0800

    vuex配合h5 session缓存，解决浏览器刷新丢失数据的问题

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,15 +1,18 @@
 import { createStore } from 'vuex'
 
+const MEMBER = "MEMBER";
+
 export default createStore({
   state: {
     // 全局变量
-    member:{ },
+    member: window.SessionStorage.get(MEMBER) || {},  // ||{}避免空指针异常
   },
   getters: {
   },
   mutations: {
     setMember( state, _member){
       state.member = _member;
+      window.SessionStorage.set(MEMBER, _member);
     }
   },
   // 任务
