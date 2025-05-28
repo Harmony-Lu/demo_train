c2a258ec057169d07fcd96867e8b199e423d6166
Harmony-Lu <3292947704@qq.com>
2025-05-28 16:42:25 +0800

    增加business业务模块

--- business/src/main/java/com/java/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/java/train/business/config/BusinessApplication.java
@@ -1,24 +1,23 @@
-package com.java.train.member.config;
+package com.java.train.business.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @SpringBootApplication
 @ComponentScan("com.java") // 帮助扫描到controller包中的文件
 @MapperScan("com.java.train.*.mapper") // 指示模块的持久层代码路径
-public class MemberApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(MemberApplication.class);
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
         Environment env = app.run(args).getEnvironment();
-        LOG.info("Member启动成功！");
+        LOG.info("Business启动成功！");
         LOG.info("测试地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
