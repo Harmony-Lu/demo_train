8a5917d6a015c68cea1270d3d2bf70d4fcecd073
Harmony-Lu <3292947704@qq.com>
2025-05-30 21:59:28 +0800

    为车次、车站、车厢增加唯一性校验

--- business/src/main/java/com/java/train/business/service/TrainCarriageService.java
+++ business/src/main/java/com/java/train/business/service/TrainCarriageService.java
@@ -1,19 +1,22 @@
 package com.java.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.java.train.business.enums.SeatColEnum;
-import com.java.train.common.resp.PageResp;
-import com.java.train.common.util.SnowUtil;
 import com.java.train.business.domain.TrainCarriage;
 import com.java.train.business.domain.TrainCarriageExample;
+import com.java.train.business.enums.SeatColEnum;
 import com.java.train.business.mapper.TrainCarriageMapper;
 import com.java.train.business.req.TrainCarriageQueryReq;
 import com.java.train.business.req.TrainCarriageSaveReq;
 import com.java.train.business.resp.TrainCarriageQueryResp;
+import com.java.train.common.exception.BusinessException;
+import com.java.train.common.exception.BusinessExceptionEnum;
+import com.java.train.common.resp.PageResp;
+import com.java.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,10 +37,15 @@ public class TrainCarriageService {
         // 自动计算出列数和总座位数
         List<SeatColEnum> seatColEnums = SeatColEnum.getColsByType(req.getSeatType());
         req.setColCount(seatColEnums.size());
-        req.setSeatCount(seatColEnums.size()*req.getRowCount());
+        req.setSeatCount(seatColEnums.size() * req.getRowCount());
 
         TrainCarriage trainCarriage = BeanUtil.copyProperties(req, TrainCarriage.class);
         if (ObjectUtil.isNull(trainCarriage.getId())) {
+            // 保存之前，先校验唯一键是否已经存在
+            TrainCarriage trainCarriageDB = selectByUnique(req.getTrainCode(), req.getIndex());
+            if(ObjectUtil.isNotEmpty(trainCarriageDB)){
+                throw new BusinessException(BusinessExceptionEnum.BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR);
+            }
             trainCarriage.setId(SnowUtil.getSnowflakeNextId());
             trainCarriage.setCreateTime(now);
             trainCarriage.setUpdateTime(now);
@@ -45,6 +53,19 @@ public class TrainCarriageService {
         } else {
             trainCarriage.setUpdateTime(now);
             trainCarriageMapper.updateByPrimaryKey(trainCarriage);
+        }
+    }
+
+    private TrainCarriage selectByUnique(String trainCode, Integer index) {
+        TrainCarriageExample trainCarriageExample = new TrainCarriageExample();
+        trainCarriageExample.createCriteria()
+                .andTrainCodeEqualTo(trainCode)
+                .andIndexEqualTo(index);
+        List<TrainCarriage> list = trainCarriageMapper.selectByExample(trainCarriageExample);
+        if(CollUtil.isNotEmpty(list)){
+            return list.get(0);
+        }else{
+            return null;
         }
     }
 
