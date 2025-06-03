d88bf59582b800c7afe05bf8c1865ef8d42fb3c6
Harmony-Lu <3292947704@qq.com>
2025-06-03 21:32:51 +0800

    为每日车次页面增加日期和车次查询条件。注意：GET请求的日期是拼接在URL里的，需要用spring自带的注解@DateTimeFormat，后端才能接收到参数

--- business/src/main/java/com/java/train/business/req/DailyTrainQueryReq.java
+++ business/src/main/java/com/java/train/business/req/DailyTrainQueryReq.java
@@ -1,12 +1,38 @@
 package com.java.train.business.req;
 
 import com.java.train.common.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainQueryReq extends PageReq {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainQueryReq{" +
+                "code='" + code + '\'' +
+                ", date=" + date +
                 "} " + super.toString();
     }
 }
