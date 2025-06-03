33016a24de0360c80fbd656f643c418c77da4152
Harmony-Lu <3292947704@qq.com>
2025-06-03 16:33:11 +0800

    增加quartz相关的12张表，增加接口来操作quartz

--- batch/src/main/java/com/java/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/java/train/batch/job/SpringBootTestJob.java
@@ -1,19 +1,19 @@
-package com.java.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-/*
-    适合单批应用，不适合集群
-    没法实施更改定时任务的状态和策略
- */
-@Component
-@EnableScheduling
-public class SpringBootTestJob {
-
-    @Scheduled(cron = "0/5 * * * * ?")
-    private void test(){
-        System.out.println("SpringBootTestJob");
-    }
-}
+//package com.java.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+///*
+//    适合单批应用，不适合集群
+//    没法实施更改定时任务的状态和策略
+// */
+//@Component
+//@EnableScheduling
+//public class SpringBootTestJob {
+//
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void test(){
+//        System.out.println("SpringBootTestJob");
+//    }
+//}
